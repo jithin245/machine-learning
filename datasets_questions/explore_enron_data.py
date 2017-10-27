d0417e3ff513d6a58a2c765de3726dcfe3bd083f
jithin jose <jithin245@gmail.com>
2017-10-27 12:32:46 +0530

    Enron Data Analysis

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,34 @@
 """
 
 import pickle
+import re
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+total_poi = len([1 for person in enron_data.keys() if enron_data[person]['poi'] is True])
+print "Total No of POIs ", total_poi
 
+with open('../final_project/poi_names.txt', 'r') as file:
+    file.readline()
+    file.readline() #Skip First 2 lines
+    
+    print "Original Total No of POIs " , len([1 for line in file if line != ''])
+
+
+print 'James Prentice\'s total stock value ', enron_data['PRENTICE JAMES']['total_stock_value']
+print 'Mails sent from wesley colwell to POI\'s ', enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+print 'CEO - total payments ', enron_data['SKILLING JEFFREY K']['total_payments']
+print 'Chairman - total payments ', enron_data['LAY KENNETH L']['total_payments']
+print 'CFO - total payments ', enron_data['FASTOW ANDREW S']['total_payments']
+
+
+print 'No. of person with salary NaN ', len([1 for person in enron_data.keys() if enron_data[person]['salary'] != 'NaN'])
+print 'No. of person with email NaN ', len([1 for person in enron_data.keys() if enron_data[person]['email_address'] != 'NaN'])
+
+total = len(enron_data.keys())
+print 'Total No of Persons ', total
+print 'No. of persons with total payments NaN ', len([1 for person,data in enron_data.items() if data['total_payments'] == 'NaN'])
+
+print 'Percent of POI with total payments NaN ' ,len([1 for person, data in enron_data.items()  \
+           if data['total_payments'] == 'NaN' and data['poi'] is True])/float(total_poi)
