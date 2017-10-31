eeeb116ca4c1b0aae9ac9fc36cb6028b0fa7e4ac
jithin jose <jithin245@gmail.com>
2017-10-31 14:09:28 +0530

    Regression - Code Changes

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,13 +37,14 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
 
+reg.fit(feature_train, target_train)
 
-
-
-
-
-
+print " slope ", reg.coef_
+print " intercept ", reg.intercept_
+print " score ", reg.score(feature_test, target_test)
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -56,14 +57,20 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
-
-
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+try:
+    plt.plot(feature_train, reg.predict(feature_train), color='r')
+except NameError:
+    pass
+
+print "New slope ", reg.coef_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
