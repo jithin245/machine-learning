84a0258a793cac76e86d66ee43b7572730331cc2
jithin jose <jithin245@gmail.com>
2017-11-02 16:12:21 +0530

    Outlier Removal Exercise

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import itertools
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -13,8 +13,15 @@ def outlierCleaner(predictions, ages, net_worths):
     
     cleaned_data = []
 
+    straighten = lambda data: list(itertools.chain.from_iterable(data))
+    error_lambda = lambda pred, net_worth: (pred-net_worth)**2
     ### your code goes here
 
-    
+    parsed_data = [(age, net_worth, error_lambda(pred, net_worth)) 
+                         for pred, age, net_worth in 
+                            zip(straighten(predictions), straighten(ages), straighten(net_worths))]
+
+    cleaned_data_len = int(0.9*len(predictions))
+    cleaned_data =  sorted(parsed_data, key=lambda data: data[2])[:cleaned_data_len]
     return cleaned_data
 
